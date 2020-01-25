8f380b87ffb1d3f650bdf7f5778c3dcdca5dccb9
rolopez23 <rob.lopez@gmail.com>
2020-01-24 19:59:38 -0800

    Updates to allow seeding

--- database/index.js
+++ database/index.js
@@ -1,10 +1,19 @@
 const mongoose = require('mongoose');
 
-var Schema = mongoose.Schema;
+const { Schema } = mongoose;
 
-//The Schema sets maxim
+
+mongoose.connect('mongodb://localhost:27017/mortgages', { useNewUrlParser: true });
+
+const db = mongoose.connection;
+// eslint-disable-next-line no-console
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  // we're connected!
+});
+
 const mortgageAdSchema = new Schema({
-  type: {type: String, enum: ['5/1 Arm', '30 Year Fixed', '15 Year Fixed']},
+  type: { type: String, enum: ['5/1 Arm', '30 Year Fixed', '15 Year Fixed'] },
   seller: String,
   NMLS: Number,
   APR: Number,
@@ -18,13 +27,13 @@ const mortgageAdSchema = new Schema({
 const listingSchema = new Schema({
   price: Number,
   region: String,
-  HOA: {type: Number, default: 0},
+  HOA: { type: Number, default: 0 },
   zip: String,
 });
 
-let Listing = mongoose.model('Listing', listingSchema);
+const Listing = mongoose.model('Listing', listingSchema);
 
-let MortgageAd = mongoose.model('Mortgage Ad', mortgageAdSchema);
+const MortgageAd = mongoose.model('Mortgage Ad', mortgageAdSchema);
 
 module.exports = {
   Listing,
--- database/seed.js
+++ database/seed.js
@@ -1,57 +1,55 @@
 const schema = require('./index.js');
 
+// drop collections
+
 const randomType = () => {
-  let types = ['5/1 Arm', '30 Year Fixed', '15 Year Fixed'];
-  let index = Math.floor(Math.random() * 3);
-  return types[index]
-}
+  const types = ['5/1 Arm', '30 Year Fixed', '15 Year Fixed'];
+  const index = Math.floor(Math.random() * 3);
+  return types[index];
+};
 
 const randomSeller = () => {
-  let banks = [['Aurora Financial, LLC', 6606],
+  const banks = [['Aurora Financial, LLC', 6606],
     ['NBKC Bank', 6706],
     ['Sebonic Financial', 23606],
     ['Homeplus Corporation', 34567],
     ['New American Funding', 6606],
     ['BNC National Bank', 418467]]
 
-  let index = Math.floor(Math.random() * 6);
+  const index = Math.floor(Math.random() * 6);
   return banks[index];
-}
+};
 
-const randomRate = () => {
-  return (Math.random() * 2 + 3).toFixed(2);
-}
+const randomRate = () => (Math.random() * 2 + 3).toFixed(2);
 
-const randomPrice = () => {
-  return (Math.random() * 2500000 + 100000)
-}
 
-const randomMinimum = () => {
-  return Math.max(Math.floor(Math.random() * 500000) - 200000, 0 );
-}
+const randomPrice = () => Math.floor((Math.random() * 2500000 + 100000));
+
+
+const randomMinimum = () => Math.max(Math.floor(Math.random() * 500000) - 200000, 0);
+
 
 const randomMaximum = () => {
   if (Math.random() > 0.9) {
     return null;
-  } else {
-    return Math.floor(Math.random() * 500000) + 500000;
   }
-}
+  return Math.floor(Math.random() * 500000) + 500000;
+};
 
 const randomRegion = () => {
-  let regions = ['HoustonMetro', 'BayMetro', 'NewYorkMetro', 'SeatleMetro', 'OmahaMetro'];
-  let index = Math.floor(Math.random() * 5);
-  return
-}
+  const regions = ['HoustonMetro', 'BayMetro', 'NewYorkMetro', 'SeattleMetro', 'OmahaMetro'];
+  const index = Math.floor(Math.random() * 5);
+  return regions[index];
+};
 
 const randomZip = () => {
-  let randomDigit = () => Math.floor(Math.random() * 10).toString();
+  const randomDigit = () => Math.floor(Math.random() * 10).toString();
   let zip = '';
-  for (var i = 0; i < 5; i++) {
+  for (let i = 0; i < 5; i += 1) {
     zip += randomDigit();
   }
-  return zip
-}
+  return zip;
+};
 
 const randomHOA = () => {
   if (Math.random() > 0.5) {
@@ -59,29 +57,31 @@ const randomHOA = () => {
   }
   return Math.floor(Math.random() * 900 + 100)
 };
+for (let i = 0; i < 100; i += 1) {
+  const listing = new schema.Listing(
+    {
+      price: randomPrice(),
+      HOA: randomHOA(),
+      zip: randomZip(),
+      region: randomRegion(),
+    },
+  );
 
-let listing = new schema.Listing(
-  {
-    price: randomPrice(),
-    HOA: randomHOA(),
-    zip: randomZip(),
-    region: randomRegion(),
-  }
-);
-
-let bank = randomSeller()
-let MortgageAd = new schema.MortgageAd(
-  {
-    type: randomType(),
-    seller: bank[0],
-    NMLS: bank[1],
-    APR: randomRate(),
-    interestRate: randomRate(),
-    minimum: randomMinimum(),
-    maximum: randomMaximum(),
-    region: randomRegion(),
-  }
-)
-console.log("Listing:", listing);
-console.log("MortgageAd:", MortgageAd);
-
+  const bank = randomSeller();
+  const mortgageAd = new schema.MortgageAd(
+    {
+      type: randomType(),
+      seller: bank[0],
+      NMLS: bank[1],
+      APR: randomRate(),
+      interestRate: randomRate(),
+      minimum: randomMinimum(),
+      maximum: randomMaximum(),
+      region: randomRegion(),
+    },
+  );
+  listing.save();
+  mortgageAd.save();
+}
+// console.log("Listing:", listing);
+// console.log("MortgageAd:", MortgageAd);
