dbd772e820c773205f86e4c9dee020e2a775a781
rolopez23 <rob.lopez@gmail.com>
2020-01-28 19:40:21 -0800

    Creates Style for card effect

---
+++ client/src/mortgageComponents/ads/AdStyles/BankRateCard.jsx
@@ -0,0 +1,12 @@
+import Styled from 'styled-components';
+
+const BankRateCard = Styled.div`
+  border: 1px solid #d1d1d1;
+  padding: 1rem 1rem;
+  width: 100%;
+  border-radius: 4px;
+  display: inline-block;
+  box-sizing: border-box;
+`;
+
+export default BankRateCard;
