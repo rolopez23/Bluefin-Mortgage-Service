5dea3cfc0a82c039cf8859c6e7dd62890160ada2
rolopez23 <rob.lopez@gmail.com>
2020-01-29 21:13:15 -0800

    Updates to have working input selection Box
    AdList is fully tested
    Write tests for Input

---
+++ client/src/mortgageComponents/ads/input/CustomSelect.jsx
@@ -0,0 +1,27 @@
+import Styled from 'styled-components'
+
+const CustomSelect = Styled.div`
+  postion: relative;
+  height: 40px;
+  width 300px;
+  border: 2px solid black;
+`
+
+const HiddenSelect = Styled.select`
+  width: 300px;
+  height:40px;
+  border: 2px solid black;
+  background: white;
+  color: black;
+  padding: 1rem;
+  font-size: 1rem;
+  border:none;
+  margin-left: 0px;
+  text-align: left;
+
+`
+
+export {
+  CustomSelect,
+  HiddenSelect,
+}
--- test/client/ads/adList.test.js
+++ test/client/ads/adList.test.js
@@ -1,21 +1,83 @@
 import React from 'react';
 import Enzyme, { mount, shallow } from 'enzyme';
 import AdList from '../../../client/src/mortgageComponents/ads/AdList.jsx';
+let testAd1 = {
+  _id: 1,
+  type: '30 Year Fixed',
+  seller: 'First Bank of Fraud',
+  NMLS: 65111,
+  APR: 5.01,
+  interestRate: 5.01,
+  minimum: 500000,
+  maximum: 500000,
+  region: 'SFBayArea',
+}
+
+const testAd2 = {
+  _id: 2,
+  type: '15 Year Fixed',
+  seller: 'First Bank of Theft',
+  NMLS: 65111,
+  APR: 5.01,
+  interestRate: 5.01,
+  minimum: 500000,
+  maximum: 500000,
+  region: 'SFBayArea',
+}
+
+const testAd3 = {
+  _id: 3,
+  type: '5/1 ARM',
+  seller: 'Mattress Bank',
+  NMLS: 65111,
+  APR: 5.01,
+  interestRate: 5.01,
+  minimum: 500000,
+  maximum: 500000,
+  region: 'SFBayArea',
+}
+
+const testAd4 = {
+  _id: 4,
+  type: '5/1 ARM',
+  seller: 'Bank of NY',
+  NMLS: 65111,
+  APR: 5.01,
+  interestRate: 5.01,
+  minimum: 500000,
+  maximum: 500000,
+  region: 'NYMetro',
+}
+
+
+let testAdList = [testAd1, testAd2, testAd3, testAd4]
 
 
 describe('Ad List', () => {
-  test('Renders the correct length', () => {
+  test('Renders the correct length based on conditional rendering', () => {
     // eslint-disable-next-line react/jsx-filename-extension
-    const wrapper = mount(<AdList ads={[1]}/>);
-    expect(wrapper).toContainMatchingElements(1, 'Ad');
+    const wrapper = mount(<AdList ads={testAdList}/>);
+    wrapper.setState({loanType: '5/1 ARM'})
+    expect(wrapper).toContainMatchingElements(2, 'Ad');
 
-    wrapper.setProps({ ads: [1, 2, 3, 4, 5] });
-    expect(wrapper).toContainMatchingElements(5, 'Ad');
+    wrapper.setState({loanType: '30 Year Fixed'});
+    expect(wrapper).toContainMatchingElements(1, 'Ad');
   });
 
   test('Sliding flexbox exists', ()=> {
-    const wrapper = shallow(<AdList ads={[1, 2, 3]}/>);
+    const wrapper = shallow(<AdList ads={testAdList}/>);
+    wrapper.setState({loanType: '5/1 ARM'})
     expect(wrapper).toContainMatchingElements(1, 'Slider');
-    expect(wrapper).toContainMatchingElements(3, 'SliderBox');
+    expect(wrapper).toContainMatchingElements(2, 'SliderBox');
   });
+
+  test('Handle Select function works', ()=> {
+    const wrapper = shallow(<AdList ads={testAdList}/>);
+    wrapper.setState({loanType: '5/1 ARM'});
+    expect(wrapper).toContainMatchingElements(2, 'SliderBox');
+    const instance = wrapper.instance();
+    instance.handleSelect('30 Year Fixed');
+    expect(wrapper).toContainMatchingElements(1, 'SliderBox');
+
+  })
 });
