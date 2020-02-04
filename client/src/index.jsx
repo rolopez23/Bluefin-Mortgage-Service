3e136bb8cacc4bd4e45ae1fc4bc9849c5aa5ef0c
rolopez23 <rob.lopez@gmail.com>
2020-02-04 11:24:26 -0800

    Entry point onto DOM is now mortgage

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 // eslint-disable-next-line import/extensions
 import App from './mortgageComponents/App.jsx';
 
-ReactDOM.render(<App listingId={1} />, document.getElementById('app'));
+ReactDOM.render(<App listingId={1} />, document.getElementById('mortgage'));
