df160aa5deceee098dce735082731f25c3c25f30
rolopez23 <rob.lopez@gmail.com>
2020-02-08 12:17:25 -0800

    Updates to stop collisions

--- webpack.prod.js
+++ webpack.prod.js
@@ -13,7 +13,7 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-react', '@babel/preset-env'],
-            plugins: [],
+            plugins:  [],
           }
         }
       }
